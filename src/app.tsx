cd4baf1455472aad5b8e641dc8de63b91e8095be
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-04-08 19:13:30 +0700

    chore: configure eslint to be more strict

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
 import { Provider as StoreProvider } from 'react-redux';
+import { router } from './router';
 import { store } from './store';
 
-export function App() {
-  return (
+export const App = () => (
     <StoreProvider store={store}>
       <RouterProvider router={router} />
     </StoreProvider>
-  );
-}
+  )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,5 @@
-export function Home() {
-  return (
+export const Home = () => (
     <div className="grid h-screen place-content-center bg-gray-950">
       <h1 className="text-4xl font-bold text-gray-600">Hello World</h1>
     </div>
-  );
-}
+  )
